59e3693af2e52432ede9e758407c70d2b9772892
Striimz <brueltheo@gmail.com>
2019-04-08 15:56:01 +0200

    Token Authentication

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -11,24 +11,33 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class TokenController extends Controller
 {
-    public function logAction($username, $password)
+    // @Route("/log/{username}/{password}", name="get_token")
+
+    /**
+     * @Route(
+     *     name="get_token",
+     *     path="/tokens/log/{username}/{password}",
+     *     methods="GET",
+     *     defaults={
+     *      "_api_resource_class"=Token::class,
+     *     }
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getTokenAction($username,$password)
     {
         $userManager = $this->container->get('fos_user.user_manager');
         $user = $userManager->findUserByUsername($username);
         $serializer = $this->get('serializer');
-
-        if ($user != null) {
-            $encoder = \Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder();
-            if ($encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt())) {
-                $jwtManager = $this->container->get('lexit_jwt_authentication.jwt_manager');
+        if ($user!=null) {
+            $encoder = new \Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder();
+            // on va encoder à la main et vérifier par rapport à celui en base
+            if ($encoder->isPasswordValid($user->getPassword(),$password,$user->getSalt())) {
+                $jwtManager = $this->container->get('lexik_jwt_authentication.jwt_manager');
                 $token = new Token($jwtManager->create($user));
-            } else {
-                $token = new Token("Bad Password");
-            }
-        } else {
-            $token = new Token("Bad User");
-        }
-
-        return JsonResponse::fromJsonString($serializer->serialize($token, "json"));
+            }else $token = new Token("Bad Mdp");
+        } else $token = new Token("Bad User");
+        return JsonResponse::fromJsonString($serializer->serialize($token,"json"));
     }
 }
--- src/Entity/Token.php
+++ src/Entity/Token.php
@@ -4,66 +4,71 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiProperty;
 
-/*  @ApiResource(
-        collectionOperations={},
-        itemOperations={
-            "get_token"={
-                {"route_name"="get_tokem"}
-                "swagger_context"={
-                    "operationId"="getToken",
-                    "parameters"={
-                        {
-                            "name" = "username",
-                            "required" = true,
-                            "type" = "string",
-                            "in" = "path",
-                            "description" = "username of the user"
-                        },
-                        {
-                            "name" = "password",
-                            "required" = true,
-                            "type" = "string",
-                            "in" = "path",
-                            "description" = "password of the user"
-                        }
-                    },
-                    "produces" = {
-                        "application/json"
-                    }
-                }
-            }
-        }    
-    ) 
-*/
+/**  
+ * @ApiResource(
+ *     collectionOperations={},
+ *     itemOperations={
+ *       "get_token"={
+ *          {"route_name"="get_token"},
+ *          "swagger_context" = {
+ *              "operationId"="getToken",
+ *              "parameters" = {
+ *                  {
+ *                      "name" = "username",
+ *                      "required" = true,
+ *                      "type" = "string",
+ *                      "in" = "path",
+ *                      "description" = "username of the user"
+ *                  },
+ *                  {
+ *                      "name" = "password",
+ *                      "required" = true,
+ *                      "type" = "string",
+ *                      "in" = "path",
+ *                      "description" = "password of the user"
+ *                  }
+ *              },
+ *              "produces" = {
+ *                 "application/json"
+ *               }
+ *          }
+ *      }
+ *     }
+ * )
+ */
 
 class Token
 {
-    /* @ApiProperty(
-            attributes={
-                "swagger_context"={"type"="string"}
-            }
-        ) 
-    */
+    /**
+     * 
+     * @ApiProperty(
+     *      attributes={
+     *         "swagger_context"={"type"="string"}
+     *     }
+     * )
+     */
     protected $token;
 
-    /* 
-        Token constructor
-        @param $token
-    */
-    public function __construct($token){
+    /** 
+     *   Token constructor
+     *   @param $token
+     */
+    public function __construct($token)
+    {
         $this->token = $token;
     }
 
-    /* 
-        @return mixed 
-    */
-    public function getToken(){
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
         return $this->token;
     }
 
-    /* 
-        @param mixed $token
-    */
+    /**  
+     *   @param mixed $token
+     */
     public function setToken($token): void
     {
         $this->token = $token;
